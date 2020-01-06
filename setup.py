3858cc62d5b7ea2e4a95c75bfa319efd0e115f35
jrasero <jrasero.daparte@gmail.com>
2020-01-06 15:04:32 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ if __name__ == "__main__":
             packages=['cmrr2bids'],
             entry_points= ENTRY_POINTS,
             install_requires = ['future>=0.16.0', 
-                                'pydicom>=1.3.0'],
+                                'pydicom>=1.3.0',
+                                'Cmrr2Log'],
             dependency_links = ['git+git://github.com/CoAxLab/Cmrr2Log#egg=Cmrr2Log']
             )
