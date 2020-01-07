cc0fb766ed8b022eb5f3604d8773c76ba3e27c29
jrasero <jrasero.daparte@gmail.com>
2020-01-07 11:19:29 -0500

    Update utils.py
    Corrected name of columns to be the first letter in capital

--- cmrr2bids/utils.py
+++ cmrr2bids/utils.py
@@ -13,10 +13,10 @@ def create_record_dict(signal, record_type, start_time, recording_sampling):
     _, n_channels = np.shape(signal)
     
     if n_channels > 1:
-        record_dict['columns'] = [record_type + str(ii) \
+        record_dict['Columns'] = [record_type + str(ii) \
                                   for ii in np.arange(1, n_channels + 1)]
     else:
-        record_dict['columns'] = [record_type]
+        record_dict['Columns'] = [record_type]
     
     record_dict['StartTime'] = start_time
     record_dict['SamplingFrequency'] = float(recording_sampling)
